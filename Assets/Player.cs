fbfd2a00bc364b6f9e64de62ede5a2fe85cef09e
Jaro  Nieuwenhuisen <jaronieuwenhuisen@gmail.com>
2026-03-18 17:57:46 +0100

    Basic building setup + goals added + player movement done

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,36 +3,63 @@ public class Player : MonoBehaviour
 {
     public float MV_SPD = 10;
     public float Rot_SPD = 30;
+    public float JumpForce = 50;
+
+    private float xRotation = 0;
 
     private Camera camera;
+    private Rigidbody rb;
+
+    private bool canJump;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         camera = Camera.main;
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotate();        
+        Rotate();
+
+        if (Physics.Raycast(transform.position, -transform.up, out RaycastHit hit, 1.2f) && Input.GetKey(KeyCode.Space))
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
     }
 
     private void FixedUpdate()
     {
         Move();
+
+        if (canJump)
+        {
+            rb.AddForce(transform.up * JumpForce, ForceMode.Impulse);
+        }
     }
 
     private void Move()
     {
         Vector3 MoveVector = new(/*0*/ Input.GetAxis("Horizontal") /**/, 0, Input.GetAxis("Vertical"));
-        MoveVector *= Time.deltaTime * MV_SPD;
-        transform.position += MoveVector;
+        MoveVector *= MV_SPD;
+        rb.AddRelativeForce(MoveVector);
     }
 
     private void Rotate()
     {
-        float rotationH = Input.GetAxis("Mouse X") * Rot_SPD * Time.deltaTime;
-        float rotationV = Input.GetAxis("Mouse Y") * Rot_SPD * Time.deltaTime;
-        camera.transform.Rotate(rotationV, rotationH, 0);
+            float mouseX = Input.GetAxis("Mouse X") * Rot_SPD * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * Rot_SPD * Time.deltaTime;
+
+            xRotation -= mouseY;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            transform.Rotate(Vector3.up * mouseX);
     }
 }
---
+++ Assets/ShadersMaterials/Goals/GoalShader.shader
@@ -0,0 +1,59 @@
+Shader "CustomGoalTexture/GoalShader"
+{
+	Properties
+	{
+		_Color("Color", Color) = (1,1,1,1)
+        _Color2("Color2", Color) = (0,1,1,1)
+        _Color3("Color3", Color) = (0,0,1,1)
+        _Iterations("Iterations", Integer) = 1
+        _Texture("Texture", 2D) = "white" {}
+	}
+
+    SubShader
+    {
+        Tags { "Queue"="Transparent" "RenderType"="Transparent" }
+        ZWrite Off
+        Blend SrcAlpha OneMinusSrcAlpha
+
+        Pass
+        {
+            Name "GoalShader"
+
+            CGPROGRAM
+            #include "UnityCustomRenderTexture.cginc"
+            #pragma vertex CustomRenderTextureVertexShader
+            #pragma fragment frag
+            #pragma target 3.0
+
+            float4      _Color;
+            float4      _Color2;
+            float4      _Color3;
+            int     _Iterations;
+            sampler2D  _Texture;
+            float4 _Texture_ST;
+
+
+            float4 frag(v2f_customrendertexture IN) : SV_Target
+            {
+                // return float4(1, 0, 0, 0.2); semi-transparent red
+
+                float2 uv = IN.localTexcoord.xy;
+                float4 color = _Color;
+                // color = float4(uv.x,uv.y,uv.y,1) * _Color;
+
+                // float PI = 3.14159265359;
+
+                float x = uv.x - .5;
+                float y = uv.y - .5;
+                // float r = sqrt(x*x + y*y);
+                // float radAngle = atan2(x,-y);
+                // float a2 = radAngle + fmod(_Time.y, 2 * PI);
+
+                color = _Color;
+               
+				return color;
+            }
+            ENDCG
+        }
+    }
+}
