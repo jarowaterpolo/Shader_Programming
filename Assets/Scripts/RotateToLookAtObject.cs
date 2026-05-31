678a8925f87895a5e0ca9d16515507c4a0cd1a9c
Jaro  Nieuwenhuisen <jaronieuwenhuisen@gmail.com>
2026-05-31 19:47:37 +0200

    added an audience that either watches the player or the ball
    a use for cross product as well as dot used another time

--- Assets/Scripts/RotateToLookAtObject.cs
+++ Assets/Scripts/RotateToLookAtObject.cs
@@ -5,10 +5,10 @@ public class RotateToLookAtObject : MonoBehaviour
 {
     [SerializeField] private Transform ObjectToLookAt;
 
-    [SerializeField] private Vector3 lookVector;
-    [SerializeField] private Vector3 crossVector;
+    private Vector3 lookVector;
+    private Vector3 crossVector;
 
-    [SerializeField] private float degreesBetweenVectors;
+    private float degreesBetweenVectors;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +21,14 @@ public class RotateToLookAtObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        lookVector = ObjectToLookAt.position - transform.position;
-        lookVector.Normalize();
+        lookVector = (ObjectToLookAt.position - transform.position).normalized *-1;
+        lookVector.y = 0;
         crossVector = Vector3.Cross(lookVector, transform.forward);
-        degreesBetweenVectors = Mathf.Asin(crossVector.magnitude / lookVector.magnitude * transform.forward.magnitude) * Mathf.Rad2Deg;
-        var targetRotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y + degreesBetweenVectors, transform.rotation.z);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime);
+
+        float dot = Vector3.Dot(transform.forward, lookVector);
+        degreesBetweenVectors = Mathf.Acos(Mathf.Clamp(dot,-1f,1f)) * Mathf.Rad2Deg;
+
+        var targetRotation = Quaternion.AngleAxis(degreesBetweenVectors, crossVector.normalized) * transform.rotation;
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
     }
 }
